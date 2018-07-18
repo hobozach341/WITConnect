6000ce34b27280394533b2ec90a89cfa1e7528b8
Zachary Gagnon <gagnonz@wit.edu>
2018-07-18 14:58:15 -0400

    Commit QRcodeViewController

--- WITConnectV3/QRCodeViewController.swift
+++ WITConnectV3/QRCodeViewController.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2018 Zachary Gagnon. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class QRCodeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func QRScannerHome(_ sender: Any) {
+        let QRGenViewController = storyboard?.instantiateViewController(withIdentifier: "QRGenViewController") as! QRGenViewController
+        present(QRGenViewController, animated:  true, completion: nil)
+        
+    }
+    @IBAction func QRCodeScan(_ sender: Any) {
+        let QRScannerController = storyboard?.instantiateViewController(withIdentifier: "QRScannerController") as! QRScannerController
+        present(QRScannerController, animated:  true, completion: nil)
+        
+    }
+    
+}
