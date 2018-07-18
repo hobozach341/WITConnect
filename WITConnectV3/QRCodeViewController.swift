9c74c2b6f29db3c015906f6312eb78b81f074969
Zachary Gagnon <gagnonz@wit.edu>
2018-07-18 14:55:11 -0400

    first commit

---
+++ WITConnectV3/QRCodeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  QRCodeViewController.swift
+//  WITConnectV3
+//
+//  Created by Zachary Gagnon on 6/29/18.
+//  Copyright © 2018 Zachary Gagnon. All rights reserved.
+//
+
+import Foundation
